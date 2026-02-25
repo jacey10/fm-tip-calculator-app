d5d6eb63b681131b5148386f4e7512db6793900f
Jacey <jaceyopara@gmail.com>
2026-02-25 15:15:59 +0100

    added data attributes to buttons

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const bill = document.querySelector('.bill--input');
+const billInput = document.querySelector('.bill--input');
 const tipContainer = document.querySelector('.main__tip--container');
+const customTipInput = document.querySelector('.custom--input');
+const peopleInput = document.getElementById('numberOfPeople');
+const errorSpan = document.getElementById('errorMessage');
+const tipAmountDisplay = document.getElementById('tipAmountPerPerson');
+const totalAmountDisplay = document.getElementById('totalAmountPerPerson');
+const resetBtn = document.getElementById('resetBtn');
+
+
+
+
+
+
+
+
+
+
