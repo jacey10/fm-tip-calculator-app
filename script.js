bb28d3b79663e13227bfbe3048cd40be9e58bdcb
Jacey <jaceyopara@gmail.com>
2026-02-27 23:52:41 +0100

    added validation functions

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const billInput = document.querySelector('.bill--input');
+const billErrorEl = document.getElementById('billErrorSpan');
 const tipContainer = document.querySelector('.main__tip--container');
 const customTipInput = document.querySelector('.custom--input');
 const peopleInput = document.getElementById('numberOfPeople');
-const errorSpan = document.getElementById('errorMessage');
+const peopleErrorEl = document.getElementById('peopleErrorSpan');
 const tipAmountDisplay = document.getElementById('tipAmountPerPerson');
 const totalAmountDisplay = document.getElementById('totalAmountPerPerson');
 const resetBtn = document.getElementById('resetBtn');
 
-let billAmount = 0;
-let tipValue = 0;
-let numPeople = 0;
+function validateBill(bill) {
+    if(bill <= 0) {
+        showError("Bill must be greater than 0");
+    } return;
+}
+
+function validatePeople(people) {
+    if(people < 1 ) {
+        showError("Can't be zero");
+    }
+}
+
+function validateCustomTip(customTip) {
+    if (customTip <= 0 ) {
+        customTipInput.classList.add('invalid');
+    }
+}
+
+function calculate() {
+    
+}
+
 
 
 
